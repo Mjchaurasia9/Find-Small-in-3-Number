73da91e5eff2dcb3a533d0c25517c0be2efd5847
Mjchaurasia9 <92732297+Mjchaurasia9@users.noreply.github.com>
2022-04-27 01:00:03 +0530

    Add files via upload

---
+++ findsmall/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a Value:\t");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter a Value:\t");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter a Value:\t");
+int num3 = int.Parse(Console.ReadLine());
+int min = 0;
+if(num1 < num2 && num1 < num3)
+{
+min = num1;
+}
+else if(num2 < num3 && num2 < num1)
+{
+min = num2;
+}
+else
+{
+min = num3;
+}
+Console.WriteLine($"Smallest Value is ----- {min}");
